a8ff4855c02229a46c93a1b21a61e5d9c3142afa
zachj95 <zachariejones@gmail.com>
2022-02-22 22:34:10 -0800

    Firstish commit

---
+++ rename.js
@@ -0,0 +1,34 @@
+const { readdirSync, mkdirSync, existsSync, rename } = require("fs");
+const { resolve } = require("path");
+
+const imageDirPath = resolve(__dirname, "./");
+
+const files = readdirSync(imageDirPath);
+
+files.forEach((file) => {
+  if (file === "rename.js" || file === "package.json") {
+    return;
+  }
+  const newName = file.split("-");
+  let newString = "";
+
+  if (newName.length > 1) {
+    newName.forEach((string, index) => {
+      if (index === 0) return;
+      if (index === 1) return (newString += string);
+      newString += `-${string}`;
+    });
+  }
+
+  if (!existsSync("./output")) {
+    mkdirSync("./output");
+  }
+
+  rename(
+    imageDirPath + `/${file}`,
+    imageDirPath + `/output/${newString}`,
+    (err) => console.log(err)
+  );
+
+  console.log(newString);
+});
